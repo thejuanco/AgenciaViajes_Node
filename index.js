deefd55d1f8b1cdf8ff8fe3760b9a775eaaa83a1
Juanco <juanffo874@gmail.com>
2024-10-23 17:45:08 -0600

    Inicia el servidor y configura nodemon

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+//Definir el puerto
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+})
