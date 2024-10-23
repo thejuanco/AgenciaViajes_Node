ceb8cc58a67bc9ff72482bb43d11935554e36c93
Juanco <juanffo874@gmail.com>
2024-10-23 17:51:32 -0600

    Crea la primera ruta para la app

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 //Definir el puerto
 const port = process.env.PORT || 4000;
 
+app.get('/start', (req, res) => {
+    res.send("Hola, bienvenido")
+})
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 })
