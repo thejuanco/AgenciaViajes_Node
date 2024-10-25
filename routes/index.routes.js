b3bfea3d1cb650a31a4f0ec3e88168cbcc8f1050
Juanco <juanffo874@gmail.com>
2024-10-24 23:08:20 -0600

    Crea las paginas con el template engine pug

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/nosotros', function(req, res) {
-    res.send('Estamos en la página de Nosotros')
+    res.render('nosotros')
 })
 
 export default router
