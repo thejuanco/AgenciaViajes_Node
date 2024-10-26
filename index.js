a95b10603a71ce717536f2b1bb47bad55f402e7f
Juanco <juanffo874@gmail.com>
2024-10-25 22:56:18 -0600

    Obtiene el año actual y lo muestra en el footer

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const port = process.env.PORT || 4000;
 
 //Habilitar Pug
 app.set('view engine', 'pug')
+//Habilitar carpeta publica
+app.use(express.static('public'))
+
+//Obtener el año actual
+app.use((req, res, next) => {
+    //res.locals.year = new Date().getFullYear(); //locals es propio de express
+    const year = new Date()
+    res.locals.actualYear = year.getFullYear()
+    next();
+})
 
 //Rutas
 app.use('/',router)
-
-//Habilitar carpeta publica
-app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
