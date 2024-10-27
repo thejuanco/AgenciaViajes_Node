f72117b4a2c2dc36927b6f6ebb596f68086e049d
Juanco <juanffo874@gmail.com>
2024-10-27 16:34:03 -0600

    Configura e instala dotenv para la lectura de las variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 //Archivo para la configuración de la base de datos
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config({path: '.env'})
 
-const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_PASS, process.env.DATABASE_USER, {
+const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS,{
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     dialect: "mysql",
