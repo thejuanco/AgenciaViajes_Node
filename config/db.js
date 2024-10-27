8e505da30a3af658a8fe805925dcfc8be590770c
Juanco <juanffo874@gmail.com>
2024-10-27 16:51:44 -0600

    formatea el codigo para su mejor comprension

--- config/db.js
+++ config/db.js
@@ -1,18 +1,23 @@
 //Archivo para la configuración de la base de datos
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
-dotenv.config({path: '.env'})
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS,{
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASS,
+  {
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     dialect: "mysql",
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 export default db;
