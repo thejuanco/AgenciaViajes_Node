eb96ec3a1d7e39300d2b0657e0758d0d38e14ab8
Juanco <juanffo874@gmail.com>
2024-10-27 16:50:28 -0600

    Formato a las rutas de la aplicacion

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,28 +1,28 @@
-import express from 'express';
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/nosotros', function(req, res) {
-    res.render('nosotros', {
-        pagina: 'Nosotros',
-    })
-})
+router.get("/nosotros", function (req, res) {
+  res.render("nosotros", {
+    pagina: "Nosotros",
+  });
+});
 
-router.get('/', (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio',
-    })
-})
+router.get("/", (req, res) => {
+  res.render("inicio", {
+    pagina: "Inicio",
+  });
+});
 
-router.get('/testimoniales', (req, res) => {
-    res.render('testimoniales', {
-        pagina: 'Testimoniales',
-    })
-})
-router.get('/viajes', (req, res) => {
-    res.render('viajes', {
-        pagina: 'Viajes',
-    })
-})
+router.get("/testimoniales", (req, res) => {
+  res.render("testimoniales", {
+    pagina: "Testimoniales",
+  });
+});
+router.get("/viajes", (req, res) => {
+  res.render("viajes", {
+    pagina: "Viajes",
+  });
+});
 
-export default router
+export default router;
